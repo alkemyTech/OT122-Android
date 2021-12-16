bcf394a1bc4a01b09c5cfd85f4d7f0319633eab1
Jeff Noelson Amady <jeff02.jn@gmail.com>
2021-12-16 13:05:49 -0300

    Sign up activity done

--- app/src/main/java/com/alkemy/ongsomosmas/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/alkemy/ongsomosmas/ui/signup/SignUpActivity.kt
@@ -2,10 +2,95 @@ package com.alkemy.ongsomosmas.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.alkemy.ongsomosmas.databinding.ActivitySignUpBinding
+import com.alkemy.ongsomosmas.utils.afterTextChanged
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private val signUpViewModel: SignUpViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setup()
     }
+
+    private fun setup() {
+        //Button enabled
+        binding.btnSinUp.isEnabled = false
+
+        //Listener call to the API
+        binding.btnSinUp.setOnClickListener {
+            Toast.makeText(this, "Calling the API", Toast.LENGTH_SHORT).show()
+        }
+
+        // Validate data
+        checkValue()
+
+        //Observer
+        signUpViewModel.signUpFormState.observe(this, Observer {
+            val signUpFormState = it ?: return@Observer
+
+            with(binding) {
+                btnSinUp.isEnabled = signUpFormState.isDataValid
+                etName.error = signUpFormState.nameError?.let(::getString)
+                etEmail.error = signUpFormState.emailError?.let(::getString)
+                etPassword.error = signUpFormState.passwordError?.let(::getString)
+                etConfirmPassword.error = signUpFormState.samePasswordError?.let(::getString)
+            }
+        })
+
+
+    }
+
+    private fun checkValue() {
+        with(binding) {
+            etName.afterTextChanged {
+                signUpViewModel.onDataChanged(
+                    etName.text.toString(),
+                    etEmail.text.toString(),
+                    etPassword.text.toString(),
+                    etConfirmPassword.text.toString()
+                )
+            }
+
+            etEmail.afterTextChanged {
+                signUpViewModel.onDataChanged(
+                    etName.text.toString(),
+                    etEmail.text.toString(),
+                    etPassword.text.toString(),
+                    etConfirmPassword.text.toString()
+                )
+            }
+
+            etPassword.afterTextChanged {
+                signUpViewModel.onDataChanged(
+                    etName.text.toString(),
+                    etEmail.text.toString(),
+                    etPassword.text.toString(),
+                    etConfirmPassword.text.toString()
+                )
+            }
+
+            etConfirmPassword.afterTextChanged {
+                signUpViewModel.onDataChanged(
+                    etName.text.toString(),
+                    etEmail.text.toString(),
+                    etPassword.text.toString(),
+                    etConfirmPassword.text.toString()
+                )
+            }
+
+
+        }
+    }
+
 }
