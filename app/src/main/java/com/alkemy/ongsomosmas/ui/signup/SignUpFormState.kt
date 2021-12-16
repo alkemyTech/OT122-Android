f3fd188a3c2c719a17965769f2b011e5674d89c3
Jeff Noelson Amady <jeff02.jn@gmail.com>
2021-12-16 13:06:25 -0300

    Sign form state done

--- app/src/main/java/com/alkemy/ongsomosmas/ui/signup/SignUpFormState.kt
+++ app/src/main/java/com/alkemy/ongsomosmas/ui/signup/SignUpFormState.kt
@@ -1,4 +1,9 @@
 package com.alkemy.ongsomosmas.ui.signup
 
-class SignUpFormState {
-}
+data class SignUpFormState(
+    val nameError: Int? = null,
+    val emailError: Int? = null,
+    val passwordError: Int? = null,
+    val samePasswordError: Int? = null,
+    val isDataValid: Boolean = false
+)
