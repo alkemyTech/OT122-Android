4c8d5deabf6d03fbd85ee36cd5b64c90827823a9
Jeff Amady <75714102+jeffamady@users.noreply.github.com>
2022-01-06 20:47:32 -0300

    OT122-32 (#14)
    * News service and repository
    
    * Provide news service and data source
    
    * Moved to news package
    
    * News view model
    
    * Call newsViewModel's getNews and observe news
    
    * @Provide
    
    * Resource string
    
    * string with translation

--- app/src/main/java/com/alkemy/ongsomosmas/di/DataModule.kt
+++ app/src/main/java/com/alkemy/ongsomosmas/di/DataModule.kt
@@ -5,6 +5,8 @@ import com.alkemy.ongsomosmas.data.Preferences
 import com.alkemy.ongsomosmas.data.PreferencesImpl
 import com.alkemy.ongsomosmas.data.contactus.ContactUsDataSource
 import com.alkemy.ongsomosmas.data.contactus.ContactUsService
+import com.alkemy.ongsomosmas.data.home.news.NewsDataSource
+import com.alkemy.ongsomosmas.data.home.news.NewsService
 import com.alkemy.ongsomosmas.data.login.LoginDataSource
 import com.alkemy.ongsomosmas.data.login.LoginService
 import com.alkemy.ongsomosmas.data.signup.SignUpRepository
@@ -66,6 +68,14 @@ class DataModule {
     fun provideLoginDataSource(loginService: LoginService): LoginDataSource {
         return LoginDataSource(loginService)
     }
+
+    @Provides
+    fun provideNewsService(@ApiAlkemy retrofit: Retrofit) =
+        retrofit.create(NewsService::class.java)
+
+    @Provides
+    fun provideNewsDataSource(newsService: NewsService): NewsDataSource =
+        NewsDataSource(newsService)
 
     @Provides
     fun providesRegisterUserUseCase(repository: SignUpRepository): RegisterUserUseCase {
