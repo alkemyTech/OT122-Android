0f33ed775ebf6c07e595ab41351d3e5389c152b3
FacuB <54460617+FacuBayo@users.noreply.github.com>
2022-01-17 16:54:00 -0300

    OT122-42 (#34)

--- app/src/main/java/com/alkemy/ongsomosmas/ui/aboutus/AboutUsAdapter.kt
+++ app/src/main/java/com/alkemy/ongsomosmas/ui/aboutus/AboutUsAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.alkemy.ongsomosmas.R
 import com.alkemy.ongsomosmas.data.model.MembersResponse
@@ -14,11 +15,10 @@ import com.squareup.picasso.Picasso
 class AboutUsAdapter(
     private val listMembers: List<MembersResponse>,
     private val onClick: (position: Int) -> Unit
-) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    inner class ViewHolder(
-        view: View
-    ) : RecyclerView.ViewHolder(view) {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
         private val binding = ItemAboutUsBinding.bind(view)
         val memberName: TextView = binding.tvAboutUsName
         val memberImage: ImageView = binding.ivAboutUsImage
@@ -26,10 +26,31 @@ class AboutUsAdapter(
         fun bind(member: MembersResponse) {
             Picasso.get().load(member.image).into(memberImage)
             memberName.text = member.name
+
+            itemView.setOnClickListener {
+                val profileImageUrl = member.image
+                val name = member.name
+                val job = member.description
+                val admissionDate = member.createdAt
+                val facebookUrl = member.facebookUrl
+                val linkedinUrl = member.linkedinUrl
+
+
+                val action =
+                    AboutUsFragmentDirections.actionAboutUsFragmentToAboutUsDetailsFragment(
+                        profileImageUrl, name,
+                        job.toString(), admissionDate, facebookUrl, linkedinUrl
+                    )
+
+                it.findNavController().navigate(action)
+            }
+
         }
+
         init {
-            view.setOnClickListener{
+            view.setOnClickListener {
                 onClick(adapterPosition)
+
             }
         }
     }
